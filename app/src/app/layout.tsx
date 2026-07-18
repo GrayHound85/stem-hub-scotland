31714ca8fda20f3de5478a40c3df9e77d361ba43
Oliver Norrie <oliver.norrie@gmail.com>
2026-07-18 17:56:31 +0100

    Added a layout.tsx file which is required. Stopped runtime error upon startup

---
+++ app/src/app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "STEM Hub Scotland",
+  description: "Connecting STEM education across Scotland",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}

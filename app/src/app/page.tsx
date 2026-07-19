bbe250ac8f21587e3d4201da98e11b81204a8bbc
phoubus <shungless@hotmail.com>
2026-07-19 13:38:26 +0100

    adding a navbar

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,3 +1,10 @@
+import NavBar from "../Components/NavBar";
+
 export default function Home() {
-  return <h1> Hello World!</h1>;
+  return (
+    <>
+      <NavBar />
+      <h1> Hello World!</h1>
+    </>
+  );
 }

8f22b2a02cf9df1aa0ac4f5b182415b965c305fa
phoubus <shungless@hotmail.com>
2026-08-19 15:01:51 +0100

    modifocations while at ollies

--- app/src/app/sign_up/page.tsx
+++ app/src/app/sign_up/page.tsx
@@ -2,7 +2,7 @@ import { requireRole } from "@c/GetAuth";
 import SignUpForm from "./SignUpForm";
 
 // wants to have a separate place that is server side not client for efficiency
-export default async function Signin() {
+export default async function SignUp() {
   await requireRole(["anon"]);
   return <SignUpForm />;
 }
